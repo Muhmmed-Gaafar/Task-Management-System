c1b2fc073e9f32fb8779ca1c28a1d5f913b90d76
Muhmmed-Gaafar <muhmmedgaafar@gmail.com>
2024-09-29 09:57:56 +0300

    update

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -16,7 +16,7 @@ class TaskFactory extends Factory
             'title' => $this->faker->sentence(),
             'description' => $this->faker->paragraph(),
             'status' => $this->faker->randomElement(['pending', 'in-progress', 'completed']),
-            'due_date' => $this->faker->dateTimeBetween('now', '+1 month')->format('Y-m-d'),
+            'due_date' => $this->faker->format('Y-m-d'),
             'user_id' => User::factory(),
             'assigned_to' => User::factory(),
         ];
